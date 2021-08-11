4ba4d48b65c15eab292f62b5072d8a8deefc85dd
Oghenovo Usiwoma <37949128+Eunovo@users.noreply.github.com>
2021-08-11 15:58:48 +0100

    Ignore environment

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 
 echo "Booting...."
 
-ENV_HOME="/home/pi/env/anti-theft"
+# ENV_HOME="/home/pi/env/anti-theft"
 APP_HOME="/home/pi/iot-security-system"
 
 # Activate env
-source "${ENV_HOME}/bin/activate"
+# source "${ENV_HOME}/bin/activate"
 cd $APP_HOME
-sudo python --version
-sudo python main.py ws://anti-theft.herokuapp.com
+sudo python3 --version
+sudo python3 main.py ws://anti-theft.herokuapp.com
