8e32ccb767f804e7bd0cde683d2d5d5f16dfc0cb
Oghenovo Usiwoma <37949128+Eunovo@users.noreply.github.com>
2021-08-11 16:05:10 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ echo "Installing...."
 
 # ENV_HOME="/home/pi/env/anti-theft"
 
-sudo apt-get update
-sudo apt-get install python3-picamera python3-gpiozero
+sudo apt-get update -y
+sudo apt-get install -y python3-picamera python3-gpiozero
 
 # Activate env
 # source "${ENV_HOME}/bin/activate"
-python3 -m pip install websockets
+python3 -m pip install websockets -y
