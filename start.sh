ccda06b51b9edddc7c9c1178aba3a8ed8af88e07
Oghenovo Usiwoma <37949128+Eunovo@users.noreply.github.com>
2021-08-11 15:44:03 +0100

    Fix env activation script

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ ENV_HOME="/home/pi/env/anti-theft"
 APP_HOME="/home/pi/iot-security-system"
 
 # Activate env
-source "${ENV_HOME}/activate"
+source "${ENV_HOME}/bin/activate"
 cd $APP_HOME
 sudo python --version
 sudo python main.py ws://anti-theft.herokuapp.com
