3d4686076b521d98f2a9b49815170c448efc630a
Eunovo <eunovo9@gmail.com>
2021-08-27 05:51:07 +0100

    .

--- logging.py
+++ logging.py
@@ -1,14 +1,12 @@
 import requests
 
 
-class Logger {
+class Logger:
     def __init__(self, url):
-        self.url = url;
+        self.url = url
 
     def log(self, message):
         try:
             requests.get(self.url, params={'nessage': message})
         except:
             print('Could not log message')
-    
-}
