844dc40e3ae0e659bd978fbb7528634c1e42c525
Eunovo <eunovo9@gmail.com>
2021-08-29 14:53:26 +0100

    Fix package installation

--- start.sh
+++ start.sh
@@ -11,5 +11,6 @@ sudo apt-get install -y python3-picamera python3-gpiozero
 
 cd $APP_HOME
 sudo python3 --version
-sudo python3 -m pip install requests websockets -y
+sudo python3 -m pip install requests
+sudo python3 -m pip install websockets
 sudo python3 main.py ws://anti-theft.herokuapp.com https://anti-theft.herokuapp.com/log
