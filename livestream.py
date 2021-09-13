ad789dbd182436e7d40a546c9b33a765d24158d6
Eunovo <eunovo9@gmail.com>
2021-09-13 05:04:01 +0100

    Wait for startStream

--- livestream.py
+++ livestream.py
@@ -34,5 +34,5 @@ async def startStream(url, camera, web_logger):
 
 async def start(server_url, camera, web_logger):
     web_logger.log("[pi] Starting Stream")
-    startStream(server_url, camera, web_logger)
+    await startStream(server_url, camera, web_logger)
     # await websockets.serve(serve(camera), "localhost", port)
