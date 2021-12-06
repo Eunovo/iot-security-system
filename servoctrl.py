9fe51e750cbc11434b8b5fe00a8fb880aba9506f
Eunovo <eunovo9@gmail.com>
2021-12-06 07:37:33 +0100

    Smooth out sound servoCtrl inputs

--- servoctrl.py
+++ servoctrl.py
@@ -7,6 +7,8 @@ class ServoCtrl(Thread):
     def __init__(self, servo):
         super().__init__()
         self.queue = Queue()
+        self.lefts = 0
+        self.rights = 0
         self.servo = servo
         self.save_path = "/home/pi/servo.dat"
         self.current_position = ''
@@ -17,22 +19,37 @@ class ServoCtrl(Thread):
     def run(self):
         while True:
             try:
-                new_position = self.queue.get()
+                if (self.lefts == self.rights):
+                    continue
+
+                new_position = 'left'
+                self.lefts -= 1
+                if (self.rights > self.lefts):
+                    new_position = 'right'
+                    self.rights -= 1
+                    self.lefts += 1
+                
                 if (self.current_position == new_position):
                     continue
+
                 self.rotate({
                     'left': 0.1, 'right': 0.2
                 }.get(new_position))
+                print('Current Position: ', self.new_position)
                 self.set_current_position(new_position)
+                
+                # pause
                 time.sleep(10)
             except Empty:
                 pass
 
     def left(self):
-        self.queue.put_nowait('left')
+        # self.queue.put_nowait('left')
+        self.lefts += 1
 
     def right(self):
-        self.queue.put_nowait('right')
+        # self.queue.put_nowait('right')
+        self.rights += 1
 
     def rotate(self, cycle):
         # actual_cycle = 0.1 + (((cycle - (-1)) / (1 - (-1))) * (0.2 - 0.1))
