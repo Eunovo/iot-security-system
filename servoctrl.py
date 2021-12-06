4084ac34b7f169f377f0b7d3aa7025abf60f9cc6
Eunovo <eunovo9@gmail.com>
2021-12-06 07:00:42 +0100

    Limit motor movement

--- servoctrl.py
+++ servoctrl.py
@@ -3,17 +3,31 @@ import time
 class ServoCtrl:
     def __init__(self, servo):
         self.servo = servo
-        initial_angle = 0
-        self.current_angle = initial_angle
+        self.save_path = "/home/pi/servo.dat"
+        self.current_position = ''
+        with open(self.save_path, 'r') as file:
+            self.current_position = file.readline()
+        print(self.current_position)
 
     def left(self):
+        if (self.current_position == 'left'):
+            return
         self.rotate(0.1)
+        self.set_current_position('left')
 
     def right(self):
+        if (self.current_position == 'right'):
+            return
         self.rotate(0.2)
+        self.set_current_position('right')
 
     def rotate(self, cycle):
         # actual_cycle = 0.1 + (((cycle - (-1)) / (1 - (-1))) * (0.2 - 0.1))
         self.servo.value = cycle
         time.sleep(0.3)
         self.servo.value = 0
+
+    def set_current_position(self, position):
+        with open(self.save_path, 'w') as file:
+            file.write(position + '\r')
+            file.flush()
