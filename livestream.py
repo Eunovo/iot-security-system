61cd878afadac3281180d1dfec5d93ed61f88779
Eunovo <eunovo9@gmail.com>
2021-12-09 15:12:48 +0100

    Don't use video port

--- livestream.py
+++ livestream.py
@@ -11,7 +11,7 @@ data_queue = queue.Queue()
 def readImageStream(camera, web_logger):
     web_logger.log('Reading stream')
     stream = io.BytesIO()
-    for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+    for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=False):
         try:
             # Rewind the stream and send the image data over the wire
             stream.seek(0)
